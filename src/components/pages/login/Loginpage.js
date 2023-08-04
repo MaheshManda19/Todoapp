f6f24e2d88d97cb22fd727b4a196279420abff82
Maheshmanda19 <mahesh.r@marketsimplified.com>
2023-08-04 14:20:40 +0530

    message

--- src/components/pages/login/Loginpage.js
+++ src/components/pages/login/Loginpage.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import './Loginpage.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Loginpage.css";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = () => {
     if (email && password) {
-      localStorage.setItem('user', JSON.stringify({ email, password }));
-      alert('Logged in successfully!');
-      navigate('/Home');
+      localStorage.setItem("user", JSON.stringify({ email, password }));
+      alert("Logged in successfully!");
+      navigate("/Home");
     } else {
-      alert('Please enter both email and password.');
+      alert("Please enter both email and password.");
     }
   };
 
   return (
-    <div className='login-container'>
-      <div className='form'>
+    <div className="login-container">
+      <div className="form">
         <h2>Login</h2>
-        
-<input
+
+        <input
           type="text"
           placeholder="Name"
           value={email}
@@ -34,7 +34,9 @@ const LoginPage = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button className='button' onClick={handleLogin}>Login</button>
+        <button className="button" onClick={handleLogin}>
+          Login
+        </button>
       </div>
     </div>
   );
